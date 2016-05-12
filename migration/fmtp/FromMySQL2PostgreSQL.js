08ad0d2b8a2879a52cc8145d60632793b19ea997
Mike <mike@mitrend.com>
2016-05-12 12:21:10 -0400

    Fixed cascade

--- migration/fmtp/FromMySQL2PostgreSQL.js
+++ migration/fmtp/FromMySQL2PostgreSQL.js
@@ -537,20 +537,25 @@ function createDataPoolTable() {
                 generateError('\t--[createDataPoolTable] Cannot connect to PostgreSQL server...\n' + error);
                 reject();
             } else {
-                let sql = 'DROP TABLE "' + self._schema + '"."data_pool_' + self._schema + self._mySqlDbName + 'cascade " IF EXISTS'; 
+                let sql = 'DROP TABLE "' + self._schema + '"."data_pool_' + self._schema + self._mySqlDbName + '" cascade IF EXISTS'; 
                 client.query(sql, err => {
                   sql = 'CREATE TABLE "' + self._schema + '"."data_pool_' + self._schema + self._mySqlDbName + '"(' + '"json" '+mapDataTypes(self._dataTypesMap, 'text') + ');';
-                  client.query(sql, err => {
-                      done();
+                  if (err) {
+                      generateError('\t--[createDataPoolTable] ' + err, sql);
+                      reject();
+                  } else {
+                    client.query(sql, err => {
+                        done();
 
-                      if (err) {
-                          generateError('\t--[createDataPoolTable] ' + err, sql);
-                          reject();
-                      } else {
-                          log('\t--[createDataPoolTable] table "' + self._schema + '"."data_pool_' + self._schema + self._mySqlDbName + '" is created...');
-                          resolve();
-                      }
-                  });
+                        if (err) {
+                            generateError('\t--[createDataPoolTable] ' + err, sql);
+                            reject();
+                        } else {
+                            log('\t--[createDataPoolTable] table "' + self._schema + '"."data_pool_' + self._schema + self._mySqlDbName + '" is created...');
+                            resolve();
+                        }
+                    });
+                  }
                 });
             }
         });
